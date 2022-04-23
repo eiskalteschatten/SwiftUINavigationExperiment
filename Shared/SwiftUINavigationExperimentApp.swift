ac1f4cc11cf3e4884b3f19f4241d078b21ddcc7c
Alex Seifert <alex@alexseifert.com>
2022-04-23 23:06:11 +0200

    Initial Commit

---
+++ Shared/SwiftUINavigationExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationExperimentApp.swift
+//  Shared
+//
+//  Created by Alex Seifert on 23.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
