21e72357ee26bcefeacfcd4a88d760d8f55c8aee
Alex Seifert <alex@alexseifert.com>
2022-04-24 16:37:14 +0200

    Add a title and a scrollview

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,40 +12,52 @@ struct ContentView: View {
     let dataItems = getData()
     
     var body: some View {
-        #if os(macOS)
-            VStack(spacing: 15.0) {
-                ForEach(dataItems, id: \.self) { dataItem in
-                    Button(dataItem.title, action: {
-                        item = dataItem
-                    })
-                }
-            }
-            .onChange(of: item) { selectedItem in
-                if let unwrappedItem = selectedItem {
-                    let window = WindowManager()
-                    window.openWindow(contentView: WindowView(item: $item), title: unwrappedItem.title, autosaveName: "WindowType\(unwrappedItem.id)")
-                }
-            }
-            .padding()
-            .frame(minWidth: 200)
-        #else
-        if item != nil {
-            WindowView(item: $item).navigationTitle(item?.title ?? "")
-                .transition(.scale)
-        }
-        else {
-            VStack(spacing: 15.0) {
-                ForEach(dataItems, id: \.self) { dataItem in
-                    Button(dataItem.title, action: {
-                        withAnimation {
-                            item = dataItem
+        VStack(alignment: .leading, spacing: 5) {
+            Text("Objects")
+                .font(.largeTitle)
+                .padding(.top)
+                .padding(.leading)
+            
+            ScrollView {
+                #if os(macOS)
+                    VStack(spacing: 15.0) {
+                        ForEach(dataItems, id: \.self) { dataItem in
+                            Button(dataItem.title, action: {
+                                item = dataItem
+                            })
                         }
-                    })
+                    }
+                    .onChange(of: item) { selectedItem in
+                        if let unwrappedItem = selectedItem {
+                            let window = WindowManager()
+                            window.openWindow(contentView: WindowView(item: $item), title: unwrappedItem.title, autosaveName: "WindowType\(unwrappedItem.id)")
+                        }
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                #else
+                if item != nil {
+                    WindowView(item: $item).navigationTitle(item?.title ?? "")
+                        .transition(.scale)
                 }
+                else {
+                    VStack(spacing: 15.0) {
+                        ForEach(dataItems, id: \.self) { dataItem in
+                            Button(dataItem.title, action: {
+                                withAnimation {
+                                    item = dataItem
+                                }
+                            })
+                        }
+                    }
+                    .padding()
+                    .transition(.scale)
+                }
+                #endif
             }
-            .transition(.scale)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        #endif
+        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
