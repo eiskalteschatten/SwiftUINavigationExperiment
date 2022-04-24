3db9913655ba878475d254a8dde3d56f5042793c
Alex Seifert <alex@alexseifert.com>
2022-04-24 16:47:48 +0200

    Add a title and scroll to the selection view and tweak the iOS animations

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,36 +12,41 @@ struct ContentView: View {
     let dataItems = getData()
     
     var body: some View {
+        #if os(macOS)
         VStack(alignment: .leading, spacing: 5) {
-            Text("Objects")
-                .font(.largeTitle)
-                .padding(.top)
-                .padding(.leading)
+            Title()
             
             ScrollView {
-                #if os(macOS)
-                    VStack(spacing: 15.0) {
-                        ForEach(dataItems, id: \.self) { dataItem in
-                            Button(dataItem.title, action: {
-                                item = dataItem
-                            })
-                        }
+                VStack(spacing: 15.0) {
+                    ForEach(dataItems, id: \.self) { dataItem in
+                        Button(dataItem.title, action: {
+                            item = dataItem
+                        })
                     }
-                    .onChange(of: item) { selectedItem in
-                        if let unwrappedItem = selectedItem {
-                            let window = WindowManager()
-                            window.openWindow(contentView: WindowView(item: $item), title: unwrappedItem.title, autosaveName: "WindowType\(unwrappedItem.id)")
-                        }
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                #else
-                if item != nil {
-                    WindowView(item: $item).navigationTitle(item?.title ?? "")
-                        .transition(.scale)
                 }
-                else {
-                    VStack(spacing: 15.0) {
+                .onChange(of: item) { selectedItem in
+                    if let unwrappedItem = selectedItem {
+                        let window = WindowManager()
+                        window.openWindow(contentView: WindowView(item: $item), title: unwrappedItem.title, autosaveName: "WindowType\(unwrappedItem.id)")
+                    }
+                }
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
+        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: .infinity, alignment: .topLeading)
+        
+        #else
+        if item != nil {
+            WindowView(item: $item).navigationTitle(item?.title ?? "")
+                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+        }
+        else {
+            VStack(alignment: .leading, spacing: 5) {
+                Title()
+                
+                ScrollView {
+                    VStack(alignment: .center, spacing: 15.0) {
                         ForEach(dataItems, id: \.self) { dataItem in
                             Button(dataItem.title, action: {
                                 withAnimation {
@@ -51,13 +56,23 @@ struct ContentView: View {
                         }
                     }
                     .padding()
-                    .transition(.scale)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-                #endif
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .transition(.slide)
+            .frame(maxWidth: .infinity, minHeight: 200, maxHeight: .infinity, alignment: .topLeading)
         }
-        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: .infinity, alignment: .topLeading)
+        #endif
+    }
+}
+
+fileprivate struct Title: View {
+    var body: some View {
+        Text("Objects")
+            .font(Font.largeTitle.bold())
+            .padding(.top)
+            .padding(.leading)
     }
 }
 
